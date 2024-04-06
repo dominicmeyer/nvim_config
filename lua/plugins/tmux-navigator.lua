42bfe4e12a53ac13446c12a4b3cf40ea971b733b
Dominic Meyer <dominic.meyer2000@gmail.com>
2024-04-06 13:20:26 +0200

    Changed to gruvbox

---
+++ lua/plugins/tmux-navigator.lua
@@ -0,0 +1,20 @@
+return {
+	{
+		"christoomey/vim-tmux-navigator",
+		commit = "38b1d0402c4600543281dc85b3f51884205674b6",
+	  cmd = {
+      "TmuxNavigateLeft",
+      "TmuxNavigateDown",
+      "TmuxNavigateUp",
+      "TmuxNavigateRight",
+      "TmuxNavigatePrevious",
+    },
+    keys = {
+      { "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
+      { "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
+      { "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
+      { "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
+      { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
+    },
+  }
+}
