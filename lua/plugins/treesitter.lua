e3037ab4ce717053f5d239a7b727e38f73824c02
Dominic Meyer <dominic.meyer2000@gmail.com>
2024-10-07 17:02:13 +0200

    Excluded faulty parser

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,12 +1,13 @@
 return {
 	{
 		"nvim-treesitter/nvim-treesitter",
-		version = "v0.9.1",
+		version = "v0.9.2",
 		build = ':TSUpdate',
 		config = function()
 			require 'nvim-treesitter.configs'.setup {
 				sync_install = false,
 				auto_install = true,
+				ignore_install = { "html" },
 
 				highlight = {
 					enable = true,
